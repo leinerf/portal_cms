a8f0204c0988568060c52e9412cede94b5ea5fd4
Freniel Zabala <frenielzabala@gmail.com>
2018-01-19 12:39:41 -0800

    its done

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,14 @@
 var express = require("express");
 var router = express.Router();
+var mongoose  = require('mongoose');
+
+var pagesSchema = mongoose.Schema({
+	title: { type : String , unique : false, required : true },
+	content: { type : String ,unique: false, required : true },
+	url: { type : String ,unique: false, required : true }
+});
+
+var pagesModel = mongoose.model('pages', pagesSchema);
 
 router.get('/',function(req,res,next){
 	res.render("dashboard");
@@ -11,6 +20,28 @@ router.get('/addpage',function(req,res,next){
 
 router.get('/editpage',function(req,res,next){
 	res.render("editPage");
+});
+
+router.post('/addpage/',function(req,res,next){
+	console.log(req.body);
+	var formData ={
+		title: req.body.title,
+		content: req.body.content,
+		url: req.body.url
+	}
+	console.log(formData);
+	pagesModel.create(formData, function(err, newUser){
+		console.log(newUser);
+		if(err){
+			console.log(err);
+			res.json(err.message);
+		} else {
+			console.log(newUser);
+			res.redirect('/')		
+		}
+	})
+	
+
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,14 @@ var express = require('express');
 var mongoose  = require('mongoose');
 var router = express.Router();
 
+//data can be urlencoded key value pairs
 var userSchema = mongoose.Schema({
-	email: String,
-	password: String
+	email: { type : String , unique : true, required : true },
+	password: { type : String ,unique: false, required : true }
 })
 
 var userModel = mongoose.model('users', userSchema);
+
 
 
 /* GET home page. */
@@ -16,12 +18,51 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/home', function(req, res, next) {
-  res.render('template');
+  res.render('landing');
 });
 
-router.get('/auth',function(req,res,next){
+router.get('/auth/login',function(req,res,next){
 	res.render('login');
-})
+});
+router.post('/auth/login',function(req,res,next){
+		console.log(req.body);
+		userModel.findOne({email: req.body.email2},function(err,foundUser){
+			console.log("it went through here");
+			if(err){
+				console.log(err);
+				res.render('login');
+			}
+			else if(foundUser === null){
+				res.render('login');
+			}
+			else if(req.body.password2 === foundUser.password){
+				console.log('successful login')
+				res.redirect('/admin');
+			}
+		});
+	
+});
+
+router.post('/auth/register',function(req,res,next){
+	console.log(req.body);
+	var formData ={
+		email: req.body.email,
+		password: req.body.password
+	}
+	console.log(formData);
+	userModel.create(formData, function(err, newUser){
+		console.log(newUser);
+		if(err){
+			console.log(err);
+			res.json(err.message);
+		} else {
+			console.log(newUser);
+			res.redirect('/admin/')		
+		}
+	})
+	
+
+});
 
 
 router.get('/test',function(req, res){
